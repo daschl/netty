fa810395ea21031ba64cdde45d040950814e8eeb
Michael Nitschinger <michael@nitschinger.at>
2013-11-05 15:55:40 +0100

    Adding first passing tests for plain and cram-md5 clients.

--- handler/src/main/java/io/netty/handler/sasl/AbstractSaslClientHandler.java
+++ handler/src/main/java/io/netty/handler/sasl/AbstractSaslClientHandler.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 package io.netty.handler.sasl;
 
 import io.netty.buffer.ByteBuf;
@@ -18,6 +33,7 @@ import javax.security.sasl.Sasl;
 import javax.security.sasl.SaslClient;
 import javax.security.sasl.SaslException;
 import java.net.SocketAddress;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -105,7 +121,7 @@ public abstract class AbstractSaslClientHandler extends ByteToMessageDecoder imp
             if (client.hasInitialResponse()) {
                 challengeResponse(client.evaluateChallenge(new byte[] {}), response);
             } else {
-                response = new ChallengeResponse(NegotiationState.SUCCESS, new byte[] {});
+                response = new ChallengeResponse(NegotiationState.CONTINUE, new byte[] {});
             }
 
             while (!client.isComplete() &&
--- handler/src/test/java/io/netty/handler/sasl/AbstractSaslClientHandlerTest.java
+++ handler/src/test/java/io/netty/handler/sasl/AbstractSaslClientHandlerTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 package io.netty.handler.sasl;
 
 import io.netty.channel.ChannelFuture;
@@ -6,23 +21,59 @@ import org.junit.Test;
 
 import javax.security.auth.callback.*;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 /**
- * Created with IntelliJ IDEA.
- * User: michael
- * Date: 05/11/13
- * Time: 13:54
- * To change this template use File | Settings | File Templates.
+ * Tests the {@link AbstractSaslClientHandler} with a dummy implementation.
  */
 public class AbstractSaslClientHandlerTest {
 
+    private static final String USERNAME = "user";
+    private static final String PASSWORD = "pass";
+    private static final String NULL = "\0";
 
     @Test
-    public void shouldNegotiateSuccessfully() throws Exception {
-        DummySaslClientHandler handler = new DummySaslClientHandler("authid", "prot", new HashMap<String, Object>());
-        EmbeddedChannel channel = new EmbeddedChannel(handler);
+    public void shouldNegotiateSuccessfullyWithCRAMMD5() throws Exception {
+        DummySaslClientHandler handler = new DummySaslClientHandler(
+            new String[] {"CRAM-MD5"},
+            USERNAME,
+            PASSWORD
+        );
+        new EmbeddedChannel(handler);
+
+        String regex = "^" + USERNAME + " " + "\\w{32}";
+        assertTrue(handler.getChallengeResult().matches(regex));
+    }
+
+    @Test
+    public void shouldNegotiateSuccessfullyWithPLAIN() throws Exception {
+        DummySaslClientHandler handler = new DummySaslClientHandler(
+                new String[] {"PLAIN"},
+                USERNAME,
+                PASSWORD
+        );
+        new EmbeddedChannel(handler);
+
+        String expected = DummySaslClientHandler.AUTH_ID + NULL + USERNAME + NULL + PASSWORD;
+        assertEquals(expected, handler.getChallengeResult());
+    }
+
+    @Test
+    public void shouldPickAlgorithmInOrder() throws Exception {
+        DummySaslClientHandler handler = new DummySaslClientHandler(
+                new String[] {"CRAM-MD5", "PLAIN"},
+                USERNAME,
+                PASSWORD
+        );
+        new EmbeddedChannel(handler);
+
+        String regex = "^" + USERNAME + " " + "\\w{32}";
+        assertTrue(handler.getChallengeResult().matches(regex));
     }
 
     /**
@@ -31,13 +82,24 @@ public class AbstractSaslClientHandlerTest {
      */
     class DummySaslClientHandler extends AbstractSaslClientHandler {
 
-        DummySaslClientHandler(String authId, String protocol, Map<String, ?> properties) {
-            super(authId, protocol, properties);
+        public static final String AUTH_ID = "authId";
+        public static final String PROTOCOL = "dummy";
+
+        private volatile String challengeResult;
+        private final String[] mechs;
+        private final String user;
+        private final String pass;
+
+        DummySaslClientHandler(String[] mechs, String user, String pass) {
+            super(AUTH_ID, PROTOCOL, new HashMap<String, Object>());
+            this.mechs = mechs;
+            this.user = user;
+            this.pass = pass;
         }
 
         @Override
         protected String[] supportedMechanisms() {
-            return new String[] {"CRAM-MD5", "PLAIN"};
+            return mechs;
         }
 
         @Override
@@ -47,11 +109,9 @@ public class AbstractSaslClientHandlerTest {
                 public void handle(Callback[] callbacks)  {
                     for (Callback callback : callbacks) {
                         if (callback instanceof NameCallback) {
-                            ((NameCallback) callback).setName("username");
+                            ((NameCallback) callback).setName(user);
                         } else if (callback instanceof PasswordCallback) {
-                            ((PasswordCallback) callback).setPassword("password".toCharArray());
-                        } else {
-                            throw new IllegalStateException("I do not speak " + callback);
+                            ((PasswordCallback) callback).setPassword(pass.toCharArray());
                         }
                     }
                 }
@@ -60,7 +120,12 @@ public class AbstractSaslClientHandlerTest {
 
         @Override
         protected void challengeResponse(byte[] evaluatedChallenge, ChallengeResponse response) {
-            System.out.println("called");
+            challengeResult = new String(evaluatedChallenge);
+            response.setState(NegotiationState.SUCCESS);
+        }
+
+        String getChallengeResult() {
+            return challengeResult;
         }
     }
 }
